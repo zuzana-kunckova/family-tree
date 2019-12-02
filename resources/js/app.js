223c391b5489e8758a70aa51abcb3a1c6e1635c7
Matt Stauffer <matt@tighten.co>
2019-12-02 15:24:47 -0500

    Move NeoVis draw into Webpack-built app.js; adjust it to use env vars

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
 require('./bootstrap');
+
+import Neovis from 'neovis.js';
+
+var viz;
+
+function draw() {
+    var config = {
+        container_id: "viz",
+        server_url: `bolt://${process.env.MIX_NEO4J_HOST}:${process.env.MIX_NEO4J_PORT}`,
+        server_user: process.env.MIX_NEO4J_USERNAME,
+        server_password: process.env.MIX_NEO4J_PASSWORD,
+        labels: {
+            "Person": {
+                caption: "name",
+            },
+        },
+        relationships: {
+            "MARRIED_TO": {
+                thickness: "weight",
+                caption: true,
+            },
+        },
+        initial_cypher: "MATCH p=()-[r:MARRIED_TO]->() MATCH (n) RETURN p, n"
+    };
+
+    viz = new Neovis(config);
+    viz.render();
+}
+
+window.onload = draw;
